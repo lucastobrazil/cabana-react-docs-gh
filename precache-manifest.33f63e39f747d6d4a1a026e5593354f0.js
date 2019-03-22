099cdf0dd083b89d418ae206d7636b6751abe83a
lucastobrazil <lucas.arundell@gmail.com>
2019-03-23 00:10:43 +0100

    Updates

---
+++ precache-manifest.33f63e39f747d6d4a1a026e5593354f0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "45c878eeba4cdd11aaa5",
+    "url": "/cabana-react-docs-gh/static/js/6.78348168.chunk.js"
+  },
+  {
+    "revision": "db65ae5681604dc740a0",
+    "url": "/cabana-react-docs-gh/static/js/0.64a72481.chunk.js"
+  },
+  {
+    "revision": "81ebfd1b103ac3d9410c",
+    "url": "/cabana-react-docs-gh/static/js/main.1e193a14.chunk.js"
+  },
+  {
+    "revision": "96f6485a6474307c87a2",
+    "url": "/cabana-react-docs-gh/static/js/runtime~main.004380d6.js"
+  },
+  {
+    "revision": "6de91e31095491bc487c",
+    "url": "/cabana-react-docs-gh/static/js/3.567602bc.chunk.js"
+  },
+  {
+    "revision": "397b38eb9687fbcdf8ef",
+    "url": "/cabana-react-docs-gh/static/js/4.9fc564b4.chunk.js"
+  },
+  {
+    "revision": "ccdced16e803fa931ea1",
+    "url": "/cabana-react-docs-gh/static/js/5.61184b49.chunk.js"
+  },
+  {
+    "revision": "33dd3bba59ab0fbb934c7adb3c074b4a",
+    "url": "/cabana-react-docs-gh/static/media/audio_tech.33dd3bba.png"
+  },
+  {
+    "revision": "fd55a58fe71c8028fc6a",
+    "url": "/cabana-react-docs-gh/static/js/7.6a937722.chunk.js"
+  },
+  {
+    "revision": "8d33e5f60a6cddc784d64aa2db41b09f",
+    "url": "/cabana-react-docs-gh/static/media/example_button.8d33e5f6.png"
+  },
+  {
+    "revision": "0da5a5fd0226f72b59d955c3419bc5d0",
+    "url": "/cabana-react-docs-gh/static/media/colors_sketch.0da5a5fd.png"
+  },
+  {
+    "revision": "e97a07e6f4a7507fbcfa778ddeef8342",
+    "url": "/cabana-react-docs-gh/static/media/input_metrics.e97a07e6.png"
+  },
+  {
+    "revision": "81ebfd1b103ac3d9410c",
+    "url": "/cabana-react-docs-gh/static/css/main.e5f2562c.chunk.css"
+  },
+  {
+    "revision": "c2c7f9d0e294d14553fb8af7b9a53413",
+    "url": "/cabana-react-docs-gh/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e10162f9a0dd705a14ce7d019e17d25d.js"
+  "/cabana-react-docs-gh/precache-manifest.33f63e39f747d6d4a1a026e5593354f0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/cabana-react-docs-gh/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
