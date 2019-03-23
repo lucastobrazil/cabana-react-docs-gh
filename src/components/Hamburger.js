1ab6ec51ce3fc736087fd3c33137b822f687b25c
lucastobrazil <lucas.arundell@gmail.com>
2019-03-23 23:02:37 +0100

    extract isOpen prop to prevent attr adding

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -4,25 +4,25 @@ import { Button } from 'cabana-react';
 import { themeGet } from 'styled-system';
 import { IconCaretDown } from 'cabanaico';
 
-const Hamburger = styled(Button)`
-  color: ${props => themeGet('colors.primary', 'purple')(props)};
-  opacity: 0.8;
-  transition: all 350ms ease;
-  transform: ${props => (props.isOpen ? `rotate(180deg)` : null)};
+const Hamburger = styled(({ isOpen, ...restProps }) => <Button {...restProps} />)`
+    color: ${props => themeGet('colors.primary', 'purple')(props)};
+    opacity: 0.8;
+    transition: all 350ms ease;
+    transform: ${props => (props.isOpen ? `rotate(180deg)` : null)};
 
-  &:hover {
-    opacity: 1;
-  }
-  &:focus {
-    outline: none;
-  }
+    &:hover {
+        opacity: 1;
+    }
+    &:focus {
+        outline: none;
+    }
 `;
 
 Hamburger.defaultProps = {
-  bg: 'transparent',
-  boxShadow: 'none',
-  px: 1,
-  children: <IconCaretDown />,
+    bg: 'transparent',
+    boxShadow: 'none',
+    px: 1,
+    children: <IconCaretDown />,
 };
 
 export default Hamburger;
