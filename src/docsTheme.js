e36739bf386f5988ca64b395467376c8a077c959
lucastobrazil <lucas.arundell@gmail.com>
2019-06-03 22:40:04 +0200

    remove other theme

--- src/docsTheme.js
+++ src/docsTheme.js
@@ -1,30 +1,15 @@
 import cabanaDefaultTheme from './theme';
 
-const bainTheme = {
-  ...cabanaDefaultTheme,
-  fonts: {
-    default: '"Graphik", sans-serif',
-  },
-  colors: {
-    primary: '#CC0000',
-  },
-  radii: {
-    none: '0',
-    small: '0px',
-    medium: '10px',
-    large: '100px',
-  },
-};
 const darkTheme = {
-  ...cabanaDefaultTheme,
+    ...cabanaDefaultTheme,
 
-  fonts: {
-    default: '"Hind", sans-serif',
-  },
-  colors: {
-    primary: 'pink',
-    background: cabanaDefaultTheme.colors.darkGrey,
-  },
+    fonts: {
+        default: '"Hind", sans-serif',
+    },
+    colors: {
+        primary: 'pink',
+        background: cabanaDefaultTheme.colors.darkGrey,
+    },
 };
 export default cabanaDefaultTheme;
-export { bainTheme, darkTheme };
+export { darkTheme };
