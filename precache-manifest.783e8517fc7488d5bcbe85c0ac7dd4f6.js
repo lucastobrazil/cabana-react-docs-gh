e9b3d2ca1527a50fa1c0a59a235953578e026741
lucastobrazil <lucas.arundell@gmail.com>
2019-06-10 00:07:45 +0200

    Updates

---
+++ precache-manifest.783e8517fc7488d5bcbe85c0ac7dd4f6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349d96ab2ec84617ab3518f1c9a4f34e",
+    "url": "/cabana-react-docs-gh/index.html"
+  },
+  {
+    "revision": "0cb0eca5fa306033c29f",
+    "url": "/cabana-react-docs-gh/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "ac68350b6ae572a7ee66",
+    "url": "/cabana-react-docs-gh/static/js/0.1a77a5d5.chunk.js"
+  },
+  {
+    "revision": "4bf6277b8971fdc8081f",
+    "url": "/cabana-react-docs-gh/static/js/3.89670118.chunk.js"
+  },
+  {
+    "revision": "97b70c332f851afa4396",
+    "url": "/cabana-react-docs-gh/static/js/4.be422fe4.chunk.js"
+  },
+  {
+    "revision": "a382d0749226639f5b12",
+    "url": "/cabana-react-docs-gh/static/js/5.91de4ea0.chunk.js"
+  },
+  {
+    "revision": "2267a8b5be76675119a2",
+    "url": "/cabana-react-docs-gh/static/js/6.ce993a1e.chunk.js"
+  },
+  {
+    "revision": "df1c200c083cc2f5c5bd",
+    "url": "/cabana-react-docs-gh/static/js/7.bb7d3a34.chunk.js"
+  },
+  {
+    "revision": "0cb0eca5fa306033c29f",
+    "url": "/cabana-react-docs-gh/static/js/main.27d6b6f5.chunk.js"
+  },
+  {
+    "revision": "d5f5e9acb85f3b93b301",
+    "url": "/cabana-react-docs-gh/static/js/runtime~main.2ea6b464.js"
+  },
+  {
+    "revision": "33dd3bba59ab0fbb934c7adb3c074b4a",
+    "url": "/cabana-react-docs-gh/static/media/audio_tech.33dd3bba.png"
+  },
+  {
+    "revision": "0da5a5fd0226f72b59d955c3419bc5d0",
+    "url": "/cabana-react-docs-gh/static/media/colors_sketch.0da5a5fd.png"
+  },
+  {
+    "revision": "8d33e5f60a6cddc784d64aa2db41b09f",
+    "url": "/cabana-react-docs-gh/static/media/example_button.8d33e5f6.png"
+  },
+  {
+    "revision": "e97a07e6f4a7507fbcfa778ddeef8342",
+    "url": "/cabana-react-docs-gh/static/media/input_metrics.e97a07e6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cabana-react-docs-gh/precache-manifest.94265eea60dab07c21f5ececf4326ff3.js"
+  "/cabana-react-docs-gh/precache-manifest.783e8517fc7488d5bcbe85c0ac7dd4f6.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cabana-react-docs-gh/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cabana-react-docs-gh/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
