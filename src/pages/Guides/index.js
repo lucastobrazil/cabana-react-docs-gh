8d4c63417893622ba5c43e2c015be198c3295938
lucastobrazil <lucas.arundell@gmail.com>
2019-06-24 18:54:26 +0200

    Upgrade 1.1.1

--- src/pages/Guides/index.js
+++ src/pages/Guides/index.js
@@ -9,56 +9,56 @@ import Theming from './Theming';
 import { HashLink as Link } from 'react-router-hash-link';
 
 const TopNavItem = styled(Box)`
-  transition: all 350ms;
-  text-decoration: none;
-  &:hover {
-    background-color: #fafafa;
-    box-shadow: 0 16px 24px rgba(48, 49, 51, 0.2);
-  }
+    transition: all 350ms;
+    text-decoration: none;
+    &:hover {
+        background-color: #fafafa;
+        box-shadow: 0 16px 24px rgba(48, 49, 51, 0.2);
+    }
 `;
 
 TopNavItem.defaultProps = {
-  boxShadow: 100,
-  bg: 'white',
-  border: '1px solid',
-  borderColor: 'transparent',
-  borderRadius: 'small',
-  color: 'primary',
-  py: 2,
-  textAlign: 'center',
-  is: Link,
-  fontWeight: 'bold'
+    boxShadow: 100,
+    bg: 'white',
+    border: '1px solid',
+    borderColor: 'transparent',
+    borderRadius: 'small',
+    color: 'primary',
+    py: 2,
+    textAlign: 'center',
+    is: Link,
+    fontWeight: 'bold',
 };
 
 const sections = [
-  { url: 'GettingStarted', name: 'Getting Started' },
-  { url: 'Styling', name: 'Styling' },
-  { url: 'Theming', name: 'Theming' },
-  { url: 'BestPractices', name: 'Best Practices' },
+    { url: 'GettingStarted', name: 'Getting Started' },
+    { url: 'Styling', name: 'Styling' },
+    { url: 'Theming', name: 'Theming' },
+    { url: 'BestPractices', name: 'Best Practices' },
 ];
 
 export default function Guides() {
-  return (
-    <React.Fragment>
-      <Section>
-        <LeadText mb={4}>Guides</LeadText>
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: '1fr 1fr 1fr 1fr',
-            gridColumnGap: 16,
-            gridRowGap: 16,
-          }}
-        >
-          {sections.map((item, key) => (
-            <TopNavItem key={key} to={`#${item.url}`} children={item.name} />
-          ))}
-        </div>
-      </Section>
-      <GettingStarted title={sections[0].url} />
-      <Styling title={sections[1].url} />
-      <Theming title={sections[2].url} />
-      <BestPractices title={sections[3].url} />
-    </React.Fragment>
-  );
+    return (
+        <Box px={2}>
+            <Section>
+                <LeadText mb={4}>Guides</LeadText>
+                <Box
+                    display="grid"
+                    style={{
+                        gridTemplateColumns: '1fr 1fr 1fr 1fr',
+                        gridColumnGap: 16,
+                        gridRowGap: 16,
+                    }}
+                >
+                    {sections.map((item, key) => (
+                        <TopNavItem key={key} to={`#${item.url}`} children={item.name} />
+                    ))}
+                </Box>
+            </Section>
+            <GettingStarted title={sections[0].url} />
+            <Styling title={sections[1].url} />
+            <Theming title={sections[2].url} />
+            <BestPractices title={sections[3].url} />
+        </Box>
+    );
 }
